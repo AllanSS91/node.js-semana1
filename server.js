b631726133ed2ef51b73f09b382f0f2c1e5c7cc5
AllanSS91 <aallansilva08@gmail.com>
2024-11-19 18:38:25 -0300

    aula_2

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 import express from "express";
 
+const posts = [
+    { descricao: 'Uma foto teste', imagem: 'https://placecats.com/mille/300/150', id: 1 },
+    { descricao: 'Gato fofo dormindo', imagem: 'https://placecats.com/cute/200/300', id: 2 },
+    { descricao: 'Gato fazendo panqueca', imagem: 'https://placecats.com/cute/200/300', id: 3 },
+  ];
+
 const app = express();
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boa Noite Devs!");
+function buscarPostPorID(id) {
+    return posts.findIndex((posts) => {
+        return posts.id === Number(id)
+    })
+}
+
+
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
+
 });
